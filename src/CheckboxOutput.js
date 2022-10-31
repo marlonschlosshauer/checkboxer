0682f8b89e82836b3253ee726558112430c3ae7d
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-10-31 20:24:22 +0100

    output: boolean -> int

--- src/CheckboxOutput.js
+++ src/CheckboxOutput.js
@@ -5,6 +5,13 @@ export const CheckboxOutput = ({ grid }) => (
   <div className="checkbox-output">
     <textarea
       className="output"
-      value={JSON.stringify(grid).replaceAll("],[", "]\n[").replaceAll(",", " ")} />
+      value={JSON
+        .stringify(grid)
+        .replaceAll("],[", "]\n[")
+        .replaceAll(",", " ")
+        // I should probably just map over it but am too lazy
+        .replaceAll("false", "0")
+        .replaceAll("true", "1")
+      } />
   </div>
 )
